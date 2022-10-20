0d54178490c109df1b89453fc5f03f1208566261
unknown <mohrafiq900@gmail.com>
2022-10-20 17:02:11 +0530

    add two problem

---
+++ Palindrome problem.js
@@ -0,0 +1,10 @@
+let str="naman"
+let new_s="";
+for(let i=str.length-1;i>=0;i--){
+	new_s=new_s+str[i];
+}
+if(new_s==str){
+	console.log("Yes");
+}else{
+	console.log("");
+}
---
+++ Prime Problem.js
@@ -0,0 +1,12 @@
+let num=13;
+let count=0;
+for(let i=0;i<=num-1;i++){
+	if(num%i==0){
+		count++;
+	}
+}
+if(count==2){
+	console.log("Yes");
+}else{
+	console.log("");
+}
